837ddc96bad80ce6ab5ea19aa3721e77be0b3695
Seth Chisamore <schisamo@chef.io>
2016-04-25 20:47:24 -0400

    Ensure specs run properly with Lita v4.6.1+

--- spec/lita/handlers/artifactory_spec.rb
+++ spec/lita/handlers/artifactory_spec.rb
@@ -1,20 +1,24 @@
 require "spec_helper"
 
 describe Lita::Handlers::Artifactory, lita_handler: true do
+  let(:endpoint) { "http://artifactory.chef.fake" }
+  let(:client) { double("Artifactory::Client") }
+
   before do
-    Lita.config.handlers.artifactory.endpoint = "http://artifactory.chef.co"
+    allow(subject).to receive(:client).and_return(client)
+    allow(client).to receive(:endpoint).and_return(endpoint)
+    # We have to explicitly call this now, see https://github.com/litaio/lita/issues/142
+    allow(described_class).to receive(:new).and_return(subject)
+    Lita.config.handlers.artifactory.endpoint = endpoint
   end
 
   it { is_expected.to route_command("artifactory repositories").to(:repos) }
   it { is_expected.to route_command("artifactory promote thing 12.0.0 from here to there").to(:promote) }
 
   describe '#artifactory promote' do
-    let(:client) { double("Artifactory::Client") }
-
     before do
-      allow(subject).to receive(:client).and_return(client)
       allow(client).to receive(:get).with("/api/build/angrychef/12.0.0").and_return(
-        "uri" => "http://artifactory.chef.co/api/build/angrychef/12.0.0",
+        "uri" => "http://artifactory.chef.fake/api/build/angrychef/12.0.0",
         "buildInfo" => {
           "name" => "angrychef",
           "number" => "12.0.0",
@@ -37,12 +41,12 @@ describe Lita::Handlers::Artifactory, lita_handler: true do
         {
           "results" => [
             {
-              "uri" => "http://artifactory.chef.co/api/storage/omnibus-current-local/com/getchef/angrychef/12.0.0/ubuntu/14.04/angrychef_12.0.0-1_amd64.deb",
+              "uri" => "http://artifactory.chef.fake/api/storage/omnibus-current-local/com/getchef/angrychef/12.0.0/ubuntu/14.04/angrychef_12.0.0-1_amd64.deb",
             },
           ],
         }
       )
-      allow(client).to receive(:get).with("http://artifactory.chef.co/api/storage/omnibus-current-local/com/getchef/angrychef/12.0.0/ubuntu/14.04/angrychef_12.0.0-1_amd64.deb").and_return(
+      allow(client).to receive(:get).with("/api/storage/omnibus-current-local/com/getchef/angrychef/12.0.0/ubuntu/14.04/angrychef_12.0.0-1_amd64.deb").and_return(
         {
           "repo" => "omnibus-current-local",
         }
@@ -58,7 +62,7 @@ describe Lita::Handlers::Artifactory, lita_handler: true do
 :metal: :ice_cream: *angrychef* *12.0.0* has been successfully promoted to the *stable* channel!
 
 You can view the promoted artifacts at:
-http://artifactory.chef.co/webapp/browserepo.html?pathId=omnibus-stable-local:com/getchef/angrychef/12.0.0
+http://artifactory.chef.fake/webapp/browserepo.html?pathId=omnibus-stable-local:com/getchef/angrychef/12.0.0
       EOH
       expect(replies.first).to eq(success_response)
     end
@@ -76,7 +80,7 @@ http://artifactory.chef.co/webapp/browserepo.html?pathId=omnibus-stable-local:co
         error_response = <<-EOH
 :scream: :skull: There was an error promoting *angrychef* *12.0.0* to the *stable* channel!
 
-Full error message from http://artifactory.chef.co:
+Full error message from http://artifactory.chef.fake:
 
 ```The Artifactory server responded with an HTTP Error 500: `#{error_message}'```
         EOH
@@ -113,7 +117,7 @@ Please verify *poop* is a valid project name and *33* is a valid version number.
 
     context "the artifacts do not exist in the current channel" do
       before do
-        allow(client).to receive(:get).with("http://artifactory.chef.co/api/storage/omnibus-current-local/com/getchef/angrychef/12.0.0/ubuntu/14.04/angrychef_12.0.0-1_amd64.deb").and_return(
+        allow(client).to receive(:get).with("/api/storage/omnibus-current-local/com/getchef/angrychef/12.0.0/ubuntu/14.04/angrychef_12.0.0-1_amd64.deb").and_return(
           {
             "repo" => "omnibus-unstable-local",
           }
@@ -134,12 +138,10 @@ The *angrychef* *12.0.0* build was not promoted to _current_ from _unstable_ bec
   end
 
   describe '#artifactory repositories' do
-    let(:client)    { double("Artifactory::Client") }
     let(:artifact1) { double("Artifactory::Resource::Artifact", key: "repo1") }
     let(:artifact2) { double("Artifactory::Resource::Artifact", key: "repo2") }
 
     before do
-      allow(subject).to receive(:client).and_return(client)
       allow(subject).to receive(:all_repos).and_return([artifact1, artifact2])
     end
 
